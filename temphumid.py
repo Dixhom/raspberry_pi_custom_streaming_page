aa8fb7b9c80b5788b27ac98c315550c787e05e0c
dixhom <kuborisho@gmail.com>
2020-06-13 16:29:43 +0900

    made it faster

--- temphumid.py
+++ temphumid.py
@@ -9,25 +9,27 @@ import datetime
 humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 4)
 
 def save_data(path, var, limit=24*60):
-	if var is not None:	
-		# get the last n-1 lines (n=limit)
-		with open(path, 'r') as f:
-			lines = f.readlines()
-			first_index = max(0, len(lines) - limit + 1)
-			# ignore empty lines
-			lines = [l.strip() for l in lines[first_index:] if l.strip()]
-		
-		str = '{0}, {1}'.format(datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S"), var)
-		lines.append(str)
-		
-		# now write'em to csv!
-		with open(path,"w") as f:
-			for line in lines:
-				f.write("%s\n" % line)
-		
-	else:
-		print 'Failed to get reading. Try again!'
-		sys.exit(1)
+        if var is None:
+                print 'Failed to get reading. Try again!'
+                sys.exit(1)
+
+        # get the last n-1 lines (n=limit)
+        with open(path, 'r') as f:
+                lines = f.readlines()
+                first_index = max(0, len(lines) - limit + 1)
+                # ignore empty lines
+                lines = [l.strip() for l in lines[first_index:] if l.strip()]
+
+        # append new data
+        str = '{0}, {1}'.format(datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S"), var)
+        lines.append(str)
+
+        # concatenate lines
+        str = '\n'.join(lines)
+
+        # now write them to csv
+        with open(path,"w") as f:
+                f.write(str)
 
 path_humid = "/home/dixhom/Adafruit_Python_DHT/examples/humid.csv"
 save_data(path_humid, humidity)
